db37801b9f9c19b0dc9e5da1889199e97fe1d2df
SayanGeeky <sayandg41@gmail.com>
2024-11-05 14:04:59 +0530

    2nd day

--- src/main.rs
+++ src/main.rs
@@ -34,18 +34,63 @@
 //     }
 // }
 
-fn main () {
-    let greetings = String::from("Hello World!");
-    println!("{}", greetings);
+// fn main () {
+//     let greetings = String::from("Hello World!");
+//     println!("{}", greetings);
 
 
-    // let greetings: &str = "Hello World!";
-    // println!("{}", greetings);
+//     // let greetings: &str = "Hello World!";
+//     // println!("{}", greetings);
 
-    // print!("{}", greetings.chars().nth(1000))
+//     // print!("{}", greetings.chars().nth(1000))
 
-   match greetings.chars().nth(1000) {
-    Some(c) => println!("Character at index 1000: {}", c),
-    None => print!("No character found at index 1000")
-   }
-}
+//    match greetings.chars().nth(1000) {
+//     Some(c) => println!("Character at index 1000: {}", c),
+//     None => print!("No character found at index 1000")
+//    }
+// }
+
+// pub fn main() {
+//     let is_even: bool = true;
+
+//     if is_even {
+//         println!("The number is even");
+//     }   else if !is_even {
+//         println!("The number is odd");
+//     }
+// }
+
+// pub fn main () {
+//     // for i in 0..10 {
+//     //     println!("{}", i)
+//     // }
+
+//     let sentence: String = String::from("my name is sayan");
+//     let first_word = get_first_word(sentence);
+//     println!("First word is: {}", first_word);
+
+// }
+
+// fn get_first_word(sentence: String) -> String {
+//     let mut ans: String = String::new();
+//     for char in sentence.chars() {
+//         ans.push_str(char.to_string().as_str());
+//         if char == ' ' {
+//             break;
+//         }
+       
+//     }
+//     ans
+// }
+
+// fn main() {
+//     let a: i32 = 10;
+//     let b: i32 = 20;
+//     let sum: i32 = do_sum(a,b);
+//     print!("Sum: {}", sum);
+
+// }
+
+// fn do_sum(a: i32, b: i32) -> i32 {
+//     return a + b;
+// }
